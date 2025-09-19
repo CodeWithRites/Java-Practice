a5eac24f27fd51ad7503dc38840d209490233e13
Ritesh Gouda <goudaritesh785@gmail.com>
2025-09-19 14:55:48 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,22 @@
+public class Operation{
+   public static void main(String args[]){
+
+  int a,b,c;
+     a=10;
+     b=20;
+     c=30;
+
+int sum=a+b;
+int sub = c-a;
+int multiplication = b*c;
+float division = b/a;
+float mod = c%a;
+
+     System.out.println("Sum is " + sum);
+     System.out.println("Substraction is " + sub);
+     System.out.println("Multiplication is " + multiplication);
+     System.out.println("Division is " + division);
+     System.out.println("Mod is " + mod); 
+  }
+}
+

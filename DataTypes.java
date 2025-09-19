f9edd491ee5a912bdbaa1587753b283923e52667
Ritesh Gouda <goudaritesh785@gmail.com>
2025-09-19 14:55:18 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+    
+        int a = 10;
+        float b = 10.0f;       
+        byte c = 100;
+        short d = 10000;       
+        long e = 125222L;
+        double f = 1.3654;
+        char g = 'B';
+        boolean h = true;      
+        String S = "Hello World !";
+
+        System.out.println("Int Value = " + a);
+        System.out.println("Float Value = " + b);
+        System.out.println("Byte Value = " + c);
+        System.out.println("Short Value = " + d);
+        System.out.println("Long Value = " + e);
+        System.out.println("Double Value = " + f);
+        System.out.println("Char Value = " + g);
+        System.out.println("Boolean Value = " + h);
+        System.out.println("String = " + S);
+    }
+}
